79b873250da9caaf1f9614c7ef81306fafd9bb44
Knut Enners <knutenners+git@gmail.com>
2016-04-29 21:06:21 +0200

    12-factor way: config with http-flag set > env var > default

--- hello/hello.go
+++ hello/hello.go
@@ -8,10 +8,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 var (
-	httpAddr = flag.String("http", ":8080", "HTTP listen address, e.g. localhost:6060")
+	flagHTTP = flag.String("http", ":8080", "HTTP listen address, e.g. localhost:6060")
 )
 
 // Says 'Hello' to last path element
@@ -21,6 +22,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	flag.Parse()
+
+	// 12factor: use env var if present, then flag
+	httpAddr := ""
+	if envHTTP := os.Getenv("HELLO_HTTP"); envHTTP != "" && *flagHTTP == ":8080" {
+		httpAddr = envHTTP
+	} else {
+		httpAddr = *flagHTTP
+	}
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(*httpAddr, nil))
+	log.Fatal(http.ListenAndServe(httpAddr, nil))
 }
