3f57e6c9e7dc2f2b7b255a6452cc3b5cba3a15f2
Knut Enners <knutenners+git@gmail.com>
2016-04-29 18:47:23 +0200

    see #2

--- hello/hello.go
+++ hello/hello.go
@@ -1,19 +1,35 @@
 // Copyright information goes here
 
-// package information just in front the package name
+// Simple hello service to cover basic go stuff
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+var (
+	port string
+)
+
+func init() {
+	const (
+		portDefault = "8080"
+		portUsage   = "HTTP listen port"
+	)
+	flag.StringVar(&port, "port", portDefault, portUsage)
+	flag.StringVar(&port, "p", portDefault, portUsage)
+}
+
+// Says 'Hello' to last path element
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s.", r.URL.Path[1:])
 }
 
 func main() {
+	flag.Parse()
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
